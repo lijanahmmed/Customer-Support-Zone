28502bc96771e66d2963900ae92c400752d39e0d
lijanahmmed <lijanahmmed@gmail.com>
2025-10-29 23:22:52 +0600

    added ticket section

--- src/Components/Ticket.jsx
+++ src/Components/Ticket.jsx
@@ -1,39 +1,66 @@
-import React from 'react';
+import React from "react";
 import { MdOutlineDateRange } from "react-icons/md";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 const Ticket = ({ ticket, progressTicket, setProgressTicket }) => {
-
-    const handleProgress = (ticket) => {
-        let updateTicket;
-        if(ticket.status == "Open"){
-            updateTicket = ticket
-            updateTicket.status = "In-Progress"
-        }
-        setProgressTicket([...progressTicket, ticket])
-        
-        toast.info("In- Progress")
+  const handleProgress = (ticket) => {
+    let updateTicket;
+    if (ticket.status == "Open") {
+      updateTicket = ticket;
+      updateTicket.status = "In-Progress";
     }
+    setProgressTicket([...progressTicket, ticket]);
 
-    return (
-        <div onClick={() => handleProgress(ticket)} className='p-3 shadow-2xl bg-white space-y-2 rounded-xl cursor-pointer'>
-            <div className='flex justify-between items-center'>
-                <p className='font-bold'>{ticket.title}</p>
-                {ticket.status == "Open" ? <p className='px-3 py-2 bg-green-200 text-green-900 rounded-4xl'>🟢Open</p> : ticket.status == "In-Progress" ? <p className='px-3 py-2 bg-yellow-100 text-yellow-800 rounded-4xl'>🟡In-Progress</p> : ""}
-            </div>
-            <p className='text-gray-500'>{ticket.description}</p>
-            <div className='flex justify-between items-center text-gray-500 text-sm'>
-                <div className='flex gap-3 items-center'>
-                    <p>#{ticket.ticketId}</p>
-                    <p className={ticket.priority == "HIGH PRIORITY" ? "text-red-600" : ticket.priority == "MEDIUM PRIORITY" ? "text-yellow-500" : "text-green-600"}>{ticket.priority}</p>
-                </div>
-                <div className='flex gap-3 items-center'>
-                    <p>{ticket.customer_name}</p>
-                    <p className='flex gap-1 items-center'><span><MdOutlineDateRange className='inline text-lg' /></span> <span> {ticket.date}</span></p>
-                </div>
-            </div>
+    toast.info("In- Progress");
+  };
+
+  return (
+    <div
+      onClick={() => handleProgress(ticket)}
+      className="p-3 shadow-2xl bg-white space-y-2 rounded-xl cursor-pointer"
+    >
+      <div className="flex justify-between items-center">
+        <p className="font-bold">{ticket.title}</p>
+        {ticket.status == "Open" ? (
+          <p className="px-3 py-2 bg-green-200 text-green-900 rounded-4xl">
+            🟢Open
+          </p>
+        ) : ticket.status == "In-Progress" ? (
+          <p className="px-3 py-2 bg-yellow-100 text-yellow-800 rounded-4xl">
+            🟡In-Progress
+          </p>
+        ) : (
+          ""
+        )}
+      </div>
+      <p className="text-gray-500">{ticket.description}</p>
+      <div className="flex justify-between items-center text-gray-500 text-sm">
+        <div className="flex gap-3 items-center">
+          <p>#{ticket.ticketId}</p>
+          <p
+            className={
+              ticket.priority == "HIGH PRIORITY"
+                ? "text-red-600"
+                : ticket.priority == "MEDIUM PRIORITY"
+                ? "text-yellow-500"
+                : "text-green-600"
+            }
+          >
+            {ticket.priority}
+          </p>
         </div>
-    );
+        <div className="flex gap-3 items-center">
+          <p>{ticket.customer_name}</p>
+          <p className="flex gap-1 items-center">
+            <span>
+              <MdOutlineDateRange className="inline text-lg" />
+            </span>{" "}
+            <span> {ticket.date}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Ticket;
--- src/Components/Tickets.jsx
+++ src/Components/Tickets.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import Ticket from './Ticket';
+import React from "react";
+import Ticket from "./Ticket";
 
 const Tickets = ({ ticketCard, progressTicket, setProgressTicket }) => {
-
-    return (
-        <div>
-            <h1 className='text-xl font-bold mb-3'>Customer Tickets</h1>
-            <div className='mt-5 grid grid-cols-1 lg:grid-cols-2 gap-3'>
-                {
-                    ticketCard.map(ticket => <Ticket progressTicket={progressTicket} setProgressTicket={setProgressTicket} key={ticket.ticketId} ticket={ticket}></Ticket>)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-xl font-bold mb-3">Customer Tickets</h1>
+      <div className="mt-5 grid grid-cols-1 lg:grid-cols-2 gap-3">
+        {ticketCard.map((ticket) => (
+          <Ticket
+            progressTicket={progressTicket}
+            setProgressTicket={setProgressTicket}
+            key={ticket.ticketId}
+            ticket={ticket}
+          ></Ticket>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Tickets;
