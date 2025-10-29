3e370348a78931d4235415a70646510a6b604773
lijanahmmed <lijanahmmed@gmail.com>
2025-10-29 23:23:49 +0600

    all code in formate

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { Suspense } from 'react'
-import './App.css'
-import Footer from './Components/Footer'
-import MainSection from './Components/MainSection'
-import Navbar from './Components/Navbar'
-import { ToastContainer } from 'react-toastify'
+import { Suspense } from "react";
+import "./App.css";
+import Footer from "./Components/Footer";
+import MainSection from "./Components/MainSection";
+import Navbar from "./Components/Navbar";
+import { ToastContainer } from "react-toastify";
 
 const fetchData = async () => {
   const res = await fetch("/tickets.json");
   return res.json();
-}
-
-
+};
 
 function App() {
   const promiseData = fetchData();
@@ -25,7 +23,7 @@ function App() {
 
       <ToastContainer></ToastContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
